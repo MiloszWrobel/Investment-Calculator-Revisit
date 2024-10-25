bdd187640298b737306fd766731911488de45fd3
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-25 11:05:26 +0100

    Updated the user input file to include a state function that holds the information of the numbers inputed into the user input field

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
         <section id="user-input">
           <h2 className="center">Enter Investment details</h2>
           <div className="input-grid">
-            <UserInput text="Initial Investment" id="initial-investment" />
-            <UserInput text="Annual Investment" id="annual-investment" />
-            <UserInput text="Expected Return" id="expected-return" />
+            <UserInput text="Initial Investment" id="initialInvestment" />
+            <UserInput text="Annual Investment" id="annualInvestment" />
+            <UserInput text="Expected Return" id="expectedReturn" />
             <UserInput text="Duration" id="duration" />
           </div>
         </section>
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 export default function UserInput({ text, id }) {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 2000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleUpdate(event) {
+    const { value } = event.target;
+
+    setUserInput((prevUserInput) => ({
+      ...prevUserInput,
+      [id]: Number(value),
+    }));
+  }
+  console.log(userInput[id]);
+
   return (
     <div className="input-group">
       <div>
         <label htmlFor={id}>{text}</label>
-        <input type="number" id={id} />
+        <input
+          type="number"
+          id={id}
+          required
+          onChange={handleUpdate}
+          value={userInput[id]}
+        />
       </div>
     </div>
   );
