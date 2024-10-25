04a9d11e09a12b0fdb2b78162888f4b05a65fbde
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-10-25 10:15:18 +0100

    Created a header and started working on the user input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <main>
+        <section id="user-input">
+          <h2 className="center">Enter Investment details</h2>
+          <UserInput />
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,10 @@
+export default function UserInput() {
+  return (
+    <div className="input-group">
+      <div>
+        <label htmlFor="initial-investment">Initial Investment</label>
+        <input type="number" id="initial-investment" />
+      </div>
+    </div>
+  );
+}
